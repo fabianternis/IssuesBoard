958b98192f76130224b3151ca328b29436951b11
Fabian ternis <f.ternis@xpsystems.eu>
2026-07-18 17:50:27 +0200

    "finish" the Styles ... (on board-view and co. ...)

--- src/views/board.php
+++ src/views/board.php
@@ -20,57 +20,58 @@
         wtf?
     </table> -->
 
-        <?php 
-            $types = ['issue', 'idea', 'todo', 'other']; 
-            
-            $groupedItems = array_fill_keys($types, []);
-            
-            foreach ($project->items as $item) {
-                $itemType = in_array($item->type, $types) ? $item->type : 'other';
-                $groupedItems[$itemType][] = $item;
-            }
-        ?>
-<div class="board">
-    <?php foreach ($types as $type): ?>
-        <div class="board-column column-<?= $type ?>">
-            <h3>
-                <?= $type ?> (<?= count($groupedItems[$type]) ?>)
-            </h3>
-            
-<!-- ToDo: Styles (some classes set already) -->
+    <?php 
+        $types = ['issue', 'idea', 'todo', 'other']; 
+        
+        $groupedItems = array_fill_keys($types, []);
+        // $groupedItems = array_fill_keys(['issue', 'idea', 'todo', 'other'], []);
+        
+        foreach ($project->items as $item) {
+            $itemType = in_array($item->type, $types) ? $item->type : 'other';
+            $groupedItems[$itemType][] = $item;
+        }
+    ?>
+    <div class="board">
+        <?php foreach ($types as $type): ?>
+            <div class="board-column column-<?= $type ?>">
+                <h3>
+                    <?= $type ?> (<?= count($groupedItems[$type]) ?>)
+                </h3>
+                
+    <!-- ToDo: Styles (some classes set already) -->
 
-            <div class="column-items">
-                <?php foreach ($groupedItems[$type] as $item): ?>
-                    <div class="item item-<?= $item->type ?>" id="item_<?= $item->id ?>">
-                        <form action="?action=update&object=item&id=<?= $item->id ?>" method="post" id="itemUpdateForm_<?= $item->id ?>">
-                            <span>ID: <?= $item->id ?></span>
-                            
-                            <label for="name_<?= $item->id ?>"></label>
-                            <input type="text" id="name_<?= $item->id ?>" name="name" placeholder="Auth Issue" value="<?= htmlspecialchars($item->name) ?>">
+                <div class="column-items">
+                    <?php foreach ($groupedItems[$type] as $item): ?>
+                        <div class="item item-<?= $item->type ?>" id="item_<?= $item->id ?>">
+                            <form action="?action=update&object=item&id=<?= $item->id ?>" method="post" id="itemUpdateForm_<?= $item->id ?>">
+                                <span>ID: <?= $item->id ?></span>
+                                
+                                <label for="name_<?= $item->id ?>"></label>
+                                <input type="text" id="name_<?= $item->id ?>" name="name" placeholder="Auth Issue" value="<?= htmlspecialchars($item->name) ?>">
 
-                            <label for="type_<?= $item->id ?>">Type</label>
-                            <select id="type_<?= $item->id ?>" name="type">
-                                <?php foreach ($types as $option): ?>
-                                    <option value="<?= $option ?>" <?= $item->type === $option ? 'selected' : '' ?>><?= ucfirst($option) ?></option>
-                                <?php endforeach ?>
-                            </select>
+                                <label for="type_<?= $item->id ?>">Type</label>
+                                <select id="type_<?= $item->id ?>" name="type">
+                                    <?php foreach ($types as $option): ?>
+                                        <option value="<?= $option ?>" <?= $item->type === $option ? 'selected' : '' ?>><?= ucfirst($option) ?></option>
+                                    <?php endforeach ?>
+                                </select>
 
-                            <label for="description_<?= $item->id ?>">Description</label>
-                            <textarea id="description_<?= $item->id ?>" name="description"><?= htmlspecialchars($item->description ?? '') ?></textarea>
+                                <label for="description_<?= $item->id ?>">Description</label>
+                                <textarea id="description_<?= $item->id ?>" name="description"><?= htmlspecialchars($item->description ?? '') ?></textarea>
 
-                            <label for="state_<?= $item->id ?>">State</label>
-                            <input type="text" id="state_<?= $item->id ?>" name="state" placeholder="In Work" value="<?= htmlspecialchars($item->state ?? '') ?>">
+                                <label for="state_<?= $item->id ?>">State</label>
+                                <input type="text" id="state_<?= $item->id ?>" name="state" placeholder="In Work" value="<?= htmlspecialchars($item->state ?? '') ?>">
 
-                            <label for="url_<?= $item->id ?>">Link</label>
-                            <input type="url" id="url_<?= $item->id ?>" name="external_url" placeholder="http://to.your/github/issue" value="<?= htmlspecialchars($item->external_url ?? '') ?>">
+                                <label for="url_<?= $item->id ?>">Link</label>
+                                <input type="url" id="url_<?= $item->id ?>" name="external_url" placeholder="http://to.your/github/issue" value="<?= htmlspecialchars($item->external_url ?? '') ?>">
 
-                            <input type="submit" value="Update Item">
-                        </form>
-                    </div>
-                <?php endforeach ?>
+                                <input type="submit" value="Update Item">
+                            </form>
+                        </div>
+                    <?php endforeach ?>
+                </div>
             </div>
-        </div>
-    <?php endforeach ?>
+        <?php endforeach ?>
     </div>
 
     <!-- <?php foreach($project->items as $item): ?>
