b67efe0f19b78e776223cab94ae19a1420467e57
Fabian ternis <f.ternis@xpsystems.eu>
2026-08-13 21:49:57 +0200

    fix: (STILL same thing for prod.)

--- src/helpers.php
+++ src/helpers.php
@@ -74,9 +74,12 @@ if (!function_exists('config')) {
 
 if (!function_exists('getCommitId')) {
     function getCommitId() {
-        if (!function_exists('shell_exec')) {
-            return '';
-        }
+        // if (!function_exists('shell_exec')) {
+        //     return '';
+        // }
+
+        return '';
+        
         // return trim((string) shell_exec('git rev-parse --short HEAD 2>/dev/null'));
         // To prevent Stuff in Prodcution
         // ToDo: is a merker for here
